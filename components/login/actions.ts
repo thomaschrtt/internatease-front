77d51de1e72b66ca151dde8442923e4e4f52b242
Charotte Thomas <charotte.thomas@etu.umontpellier.fr>
2024-12-09 20:16:34 +0100

    google auth

--- components/login/actions.ts
+++ components/login/actions.ts
@@ -57,6 +57,7 @@ export async function signInWithGoogle() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: "google",
         options: {
+            redirectTo: 'http://localhost:3000/auth/callback',
             queryParams: {
                 access_type: "offline",
                 prompt: "consent",
